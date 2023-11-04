7ffd980b6b33dc753e59d4173942a666550a3772
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-11-04 16:57:32 +0200

    add title, btns and Statistics comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,27 @@
-export const App = () => {
-  return <div></div>;
-};
+import { Component } from 'react';
+import { Statistics } from './Statistics/Statistic';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <>
+          {Object.keys(this.state).map(key => (
+            <button>{key}</button>
+          ))}
+        </>
+        <Statistics
+          good={this.state.good}
+          neutral={this.state.neutral}
+          bad={this.state.bad}
+        />
+      </div>
+    );
+  }
+}
